559789da9b8731f1977d19051c2ae49cbb251048
dkaliazin <dkaliazin@gmail.com>
2024-08-17 15:51:57 +0300

    start

--- vite-project/src/App/App.jsx
+++ vite-project/src/App/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SearchBar from '../SearchBar/SearchBar'
 import css from './App.module.css'
 import { ErrorMessage } from 'formik';
 import ImageGallery from '../ImageGallery/ImageGallery';
+import axios from 'axios';
 function App() {
   const [images, setImages] = useState([]);
   const [topicName, setTopicName] = useState("");
   const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [imageModalWindow, setImageModalWindow] = useState(false);
   const [modalData, setModalData] = useState({});
   const handleSearch = async (newTopicName) => {
@@ -14,13 +17,31 @@ function App() {
     setPage(1);
     setTopicName(newTopicName);
   }
+useEffect(() => {
+  const getImages = async () => {
+    try {
+      setLoading(true);
+      const results = await axios.get(`https://api.unsplash.com/search/photos?query=${topicName}&page=${page}&client_id=P7ByciXJKPkJC46zpMWNVK8C8nALBCKpESinSOr59DI`);
+      setImages((prev) => [...prev, ...results.data.results]);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (topicName) {
+    getImages();
+  }
+}, [topicName, page]);
 
   const handleImageModalWindow = (modalData) => {
     setImageModalWindow(!imageModalWindow);
     setModalData(modalData);
   }
   return (
-    <div>
+    <div className={css.container}>
+      
       <SearchBar onSearch={handleSearch} />
       {images.length > 0 && (
         <ImageGallery images={images} openModalWindow={handleImageModalWindow}/>
--- vite-project/src/SearchBar/SearchBar.jsx
+++ vite-project/src/SearchBar/SearchBar.jsx
@@ -1,22 +1,24 @@
 import { Formik, Form, Field } from "formik";
 import css from "./SearchBar.module.css";
-export default function SearchBar({ onSearch }) {
-    <header>
-        <Formik
-         initialValues={{ topicName: "" }}
-            onSubmit={(values, actions) => {
-            onSearch(values.topicName);
-            actions.resetForm();
-        }}>
-            <Form>
-                <Field
-                    type="text"
-                    name="topicName"
-                    autoComplete="off"         
-                    placeholder="Search by name"
-                />
-                <button type="submit">Search</button>
-            </Form>
-        </Formik>
-    </header>
+export default function SearchBar(onSearch) {
+    return (
+        <header>
+            <Formik
+                initialValues={{ topicName: "" }}
+                onSubmit={(values, actions) => {
+                    onSearch(values.topicName);
+                    actions.resetForm();
+                }}>
+                <Form>
+                    <Field
+                        type="text"
+                        name="topicName"
+                        autoComplete="off"
+                        placeholder="Search by name"
+                    />
+                    <button type="submit">Search</button>
+                </Form>
+            </Formik>
+        </header>
+    );
 }
